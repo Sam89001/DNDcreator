1258e3ec50e0f69fc86494e3b9e0d3a248686124
Samuel Cole <samscreativespace@gmail.com>
2024-01-04 18:09:25 +0000

    added login screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import './css/Site.css';
+import './css/Animations.css';
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div className="row d-flex container-fluid gx-0">
+
+          <div className="col-8" >
+            <img className="img-fluid d-block w-100 cover-image" src="/images/DND Image 2.jpg" />
+          </div>
+
+          <div className="col-4 page-fade-in">
+            <div className="row justify-content-center align-content-center">
+
+              <div className="col-12 d-flex justify-content-center align-items-center">
+                <img className="img-fluid" src="/images/Logo.png" ></img>
+              </div>
+
+              <div className="col-12 text-center">
+                <header className="header">Login</header>
+              </div>
+
+              <div className="col-6 d-flex align-items-center justify-content-center">
+                <div className="row">
+
+                  <div className="col-6 d-flex justify-content-center align-items-center text-center">
+                    <a className="cover-buttons">Register</a>
+                  </div>
+
+                  <div className="col-6 d-flex justify-content-center align-items-center text-center" >
+                    <a className="cover-buttons">Forgot Password</a>
+                  </div>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+
+        </div>
+      </div>
   );
 }
 
