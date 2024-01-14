aea312102470384a1a51879cbc82df73eee02270
Samuel Cole <samscreativespace@gmail.com>
2024-01-14 17:57:27 +0000

    implemented middleware

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@
   const mongoose = require('mongoose');
   const cors = require('cors');
   const dotenv = require('dotenv').config();
+  const cookieParser = require('cookie-parser');
   
   const App = express();
 
@@ -13,8 +14,10 @@
 		origin: 'http://localhost:3000'
   }));
 
+  //middleware
   App.use(express.json());
-
+  App.use(cookieParser());
+  App.use(express.urlencoded({extended: false}));
 
   // Database Connections
   mongoose.connect(process.env.MONGO_URL, {
