bd9043753b096c7d60471734b418e7e89d693660
Samuel Cole <samscreativespace@gmail.com>
2024-01-17 15:29:04 +0000

    added new controller and put request to successfully update data

--- api/routes/homeRoutes.js
+++ api/routes/homeRoutes.js
@@ -3,9 +3,48 @@ const cors = require('cors');
 const router = express.Router();
 const jwt = require('jsonwebtoken')
 
-router.put('/Home/AccountEdit', async(req, res) => {
-	
-})
+const { hashedPassword, comparePassword } = require('../helpers/auth')
+const RegisterSchema = require('../models/RegisterSchema');
+const { default: toast } = require('react-hot-toast');
+const { json } = require('react-router-dom');
+
+//cors middleware to resolve cors issues
+router.use(
+	cors({
+			credentials: true,
+			origin: 'http://localhost:3000'
+	})
+)
+
+
+router.put('/AccountEdit', async (req, res) => {
+  try {
+    const { firstName, email, password, id } = req.body;
+    const hashPassword = await hashedPassword(password);
+
+    const updateUserDetails = await RegisterSchema.findByIdAndUpdate(
+      id,
+      {
+        $set: { email: email, firstName: firstName, password: hashPassword }, 
+      },
+      { new: true }
+    );
+
+    if (!updateUserDetails) {
+      return res.json({
+        error: 'Error updating user data',
+      });
+    }
+
+    res.json({
+      success: true,
+      user: updateUserDetails,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 
 module.exports = router;
--- src/components/Forms/ChangeUserDetailsForm.js
+++ src/components/Forms/ChangeUserDetailsForm.js
@@ -21,78 +21,85 @@ function ChangeUserDetailsForm({userData}) {
 			firstName: '',
 			email: '',
 			password: '',
+			id: '',
 		})
 
 		useEffect(() => {
-			// Update the state with user data if userData is available
 			if (userData) {
 				setData({
 					email: userData.email || '',
 					firstName: userData.name || '',
-					password: userData.password, // Assuming you don't want to display the password in the form
+					password: userData.password || '',
+					id: userData.id 
 				});
 			}
 		}, [userData]);
-
-    const changeUserDetails = async (e) => {
-        e.preventDefault();
-        const {email, firstName, password} = data
-					try {
-						//Posts the data
-						const {data} = await axios.post('http://localhost:4000/', {
-							firstName, email, password
-						})
-
-						//toast notification
-						if (data.error) {
-							toast.error(data.error)
-						} else {
-							setData({})
-							toast.success('Successfully updated user details')
-							navigate('/Home')
-						}
-					} catch (error) {
-						console.log(error)
-					}
-    }
+	
+		const changeUserDetails = async (e) => {
+			e.preventDefault();
+			const { email, firstName, password, id } = data;
+	
+			try {
+				const { data } = await axios.put('http://localhost:4000/Home/AccountEdit', {
+					firstName,
+					email,
+					password,
+					id,
+				});
+	
+				if (data.error) {
+					toast.error(data.error);
+				} else {
+					setData({});
+					toast.success('Successfully updated user details');
+					navigate('/Home');
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		};
     
     //html
 
     return (
 
-        <form onSubmit={changeUserDetails}>
-    <div className="row d-flex justify-content-center align-items-center" style={{ maxWidth: "1400px" }}>
-        <div className="col-6 mb-4 d-flex align-items-center justify-content-center">
-            <input className="change-details-field" type="text" id="username" name="username" placeholder="Email" 
-						 value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })}/>
-        </div>
+      <form onSubmit={changeUserDetails}>
+    		<div className="row d-flex justify-content-center align-items-center" style={{ maxWidth: "1400px" }}>
+					{/* <h1>{data.id}</h1> */}
 
-        <div className="col-6 mb-4 d-flex align-items-center justify-content-center">
-            <input className="change-details-field" type="text" id="conf-username" name="conf-username" placeholder="Confirm Email" 
-						value={data.email}/>
-        </div>
+					<div className="col-6 mb-4 d-flex align-items-center justify-content-center">
+							<label> Email </label>
+							<input className="change-details-field" type="text" placeholder={data.email}/>
+					</div>
 
-        <div className="col-6 mb-4 d-flex align-items-center justify-content-center">
-            <input className="change-details-field" id="password" name="password" placeholder="Password" 
-						/>
-        </div>
+					<div className="col-6 mb-4 d-flex align-items-center justify-content-center">
+							<label> Confirm Email </label>
+							<input className="change-details-field" type="text" placeholder={data.email} 
+							onChange={(e) => setData({ ...data, email: e.target.value })}/>
+					</div>
 
-        <div className="col-6 mb-4 d-flex align-items-center justify-content-center">
-            <input className="change-details-field" id="conf-password" name="conf-password" placeholder="Confirm Password" 
-						/>
-        </div>
+					<div className="col-6 mb-4 d-flex align-items-center justify-content-center">
+							<label> Password </label>
+							<input className="change-details-field" type="text"  placeholder={data.password}/>
+					</div>
 
-        <div className="col-6 d-flex align-items-center justify-content-center" style={{ paddingBottom: "100px" }}>
-            <input className="change-details-field" type="text" id="name" name="name" placeholder="First Name" 
-						value={data.firstName}/>
-        </div>
+					<div className="col-6 mb-4 d-flex align-items-center justify-content-center">
+							<label> Confirm Password </label>
+							<input className="change-details-field" type="text" placeholder={data.password}
+							onChange={(e) => setData({ ...data, password: e.target.value })}/>
+					</div>
 
-        <div className="col-12 mb-4 d-flex align-items-center justify-content-center">
-            <button className="login-button" type="submit">Update</button>
-        </div>
-    </div>
-</form>
+					<div className="col-6 d-flex align-items-center justify-content-center" style={{ paddingBottom: "100px" }}>
+							<label> Name </label>
+							<input className="change-details-field" type="text" placeholder={data.firstName}
+							onChange={(e) => setData({ ...data, firstName: e.target.value })}/>
+					</div>
 
+					<div className="col-12 mb-4 d-flex align-items-center justify-content-center">
+							<button className="login-button" type="submit">Update</button>
+					</div>
+    		</div>
+			</form>
 
     )
 }
