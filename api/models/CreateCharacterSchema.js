8d03128e59a7bdcd41000156691a5024d32968ea
Samuel Cole <samscreativespace@gmail.com>
2024-01-19 15:16:25 +0000

    fixed database issues

--- api/models/CreateCharacterSchema.js
+++ api/models/CreateCharacterSchema.js
@@ -4,11 +4,9 @@ const Schema = mongoose.Schema;
 const CreateCharacterSchema = new mongoose.Schema({
   userId: {
 		type: String,
-		required: true
   },
   characterName: {
     type: String,
-    unique: true
   }
     
 })
