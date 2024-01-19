7b9d77e12e2093f6033bf712529283c2a46210a6
Samuel Cole <samscreativespace@gmail.com>
2024-01-19 12:52:30 +0000

    fully working unstyled popup

--- src/components/Components/PopUp.js
+++ src/components/Components/PopUp.js
@@ -4,23 +4,16 @@ import '../../css/Animations.css';
 function PopUp({closePopUp}) {
 
   return (
-    <div className="pop-up">
-      {/* PopUp content */}
-        <div
-          style={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <button onClick={closePopUp}>Close me</button>
-          {/* Your other PopUp content goes here */}
-          {/* ... */}
-        </div>
+    <div className='pop-up-overlay'>
+      <div className="pop-up">
+        {/* PopUp content */}
+
+            <button onClick={closePopUp}>Close me</button>
+            {/* Your other PopUp content goes here */}
+            {/* ... */}
+      </div>
     </div>
+
   );
 }
 
