a8957c4e0607da850719bb03e4308a80660682e2
Samuel Cole <samscreativespace@gmail.com>
2024-03-01 18:02:04 +0000

    working spells but spell slots needs implementation

--- src/components/Components/DndSheetTwo.js
+++ src/components/Components/DndSheetTwo.js
@@ -53,15 +53,27 @@ return (
   </div>
 
   <div className="absolute-div dnd-sheet-noflex" style={{  backgroundColor: 'transparent', top: '23.3%', left: '5%', width: '25%', height: '13%', fontSize: '0.59vw' }}>
-    
-		{loadCharacterSpells.map(spell => (
+		{loadCharacterSpells
+		.filter(spell => spell.characterSpellLevel === '0')
+		.map(spell => (
 			<div className='d-flex justify-content-between' key={spell._id} style={{display: 'block', width: '100%'}} >{spell.characterSpellName}
 				<button className='delete-property-button' style={{width: '10%'}} 
 				onClick={(e) => deleteItem(e, spell._id, 'http://localhost:4000/CreateCharacter/DeleteSpell/')}>X</button>
 			</div>
 		))}
-  
 	</div>
+
+	<div className="absolute-div dnd-sheet-noflex" style={{  backgroundColor: 'transparent', top: '45%', left: '6.5%', width: '23.5%', height: '13%', fontSize: '0.59vw' }}>
+		{loadCharacterSpells
+		.filter(spell => spell.characterSpellLevel === '1')
+		.map(spell => (
+			<div className='d-flex justify-content-between' key={spell._id} style={{display: 'block', width: '100%'}} >{spell.characterSpellName}
+				<button className='delete-property-button' style={{width: '10%'}} 
+				onClick={(e) => deleteItem(e, spell._id, 'http://localhost:4000/CreateCharacter/DeleteSpell/')}>X</button>
+			</div>
+		))}
+	</div>
+
 
 	<img className="img-fluid character-sheet" src={DndSheetImage} alt="Character Image" style={{minWidth: '450px', width: '90%'}}/>
 </div>
