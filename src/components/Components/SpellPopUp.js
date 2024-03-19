98247b48cbe3d13976ceb6d47cc37cf03c16b225
Samuel Cole <samscreativespace@gmail.com>
2024-03-19 19:43:51 +0000

    working pop up

--- src/components/Components/SpellPopUp.js
+++ src/components/Components/SpellPopUp.js
@@ -23,7 +23,7 @@ return(
         <div className='col-12 text-center' style={{ paddingBottom: '20px', font: '400 2vw Grenze Gotisch, sans-serif' }}>Spell Details</div>
         <div className='col-12 row justify-content-center' style={{paddingBottom: '20px'}}>
 
-          <div className='col-7 row d-flex justify-content-between' style={{paddingBottom: '10px'}}>
+          <div className='col-7 row d-flex justify-content-between' style={{paddingBottom: '20px'}}>
             <div className='col-12 d-flex justify-content-between'>
               <div className='text-center spell-pop-up-title-field' style={{ width: '18%' }}>Spell Level</div>
               <div className='text-center spell-pop-up-title-field' style={{ width: '80%' }}>Spell Name</div>
@@ -35,7 +35,7 @@ return(
             </div>
           </div>
 
-          <div className='col-5 row d-flex justify-content-between' style={{paddingBottom: '10px'}}>
+          <div className='col-5 row d-flex justify-content-between' style={{paddingBottom: '20px'}}>
 
             <div className='col-12 d-flex justify-content-between' >
               <div className='text-center spell-pop-up-title-field' style={{ width: '48%' }}>Spell Cast Time</div>
@@ -49,7 +49,7 @@ return(
 
           </div>
 
-          <div className='col-12 row d-flex' style={{paddingBottom: '10px'}}>
+          <div className='col-12 row d-flex' style={{paddingBottom: '40px'}}>
 
             <div className='col-12 text-center'>
               <div className='spell-pop-up-title-field'>Spell Description</div>
@@ -66,10 +66,10 @@ return(
           <div className='col-12 row d-flex' style={{width: '98%'}}>
 
             <div className='col-12 d-flex justify-content-between'>
-              <div className='text-center spell-pop-up-title-field' style={{ width: '24%' }}>Spell Level</div>
-              <div className='text-center spell-pop-up-title-field' style={{ width: '24%' }}>Spell Level</div>
-              <div className='text-center spell-pop-up-title-field' style={{ width: '24%' }}>Spell Level</div>
-              <div className='text-center spell-pop-up-title-field' style={{ width: '24%' }}>Spell Level</div>
+              <div className='text-center spell-pop-up-title-field' style={{ width: '24%' }}>Spell Duration</div>
+              <div className='text-center spell-pop-up-title-field' style={{ width: '24%' }}>Spell Save</div>
+              <div className='text-center spell-pop-up-title-field' style={{ width: '24%' }}>Spell School</div>
+              <div className='text-center spell-pop-up-title-field' style={{ width: '24%' }}>Spell Range/Area</div>
             </div>
 
           </div>
@@ -83,6 +83,13 @@ return(
               <div className='text-center spell-pop-up-field' style={{ width: '24%' }}>{spell.characterSpellRangeArea}</div>
             </div>
 
+          </div>
+
+          <div className='col-12 row d-flex' style={{ width: '98%' }}>
+            <div className="d-flex justify-content-between" style={{ width: '100%' }}>
+              <button className="spell-pop-up-button">Edit</button>
+              <button className="spell-pop-up-button">Delete</button>
+            </div>
           </div>
 
         </div>
