7585f9168067af83049e84bbf470ad1636d8373f
Samuel Cole <samscreativespace@gmail.com>
2024-03-21 19:01:31 +0000

    proper structuring and preparing to add in user details

--- src/components/Views/PlaySessionScreens/PlaySessionScreen.js
+++ src/components/Views/PlaySessionScreens/PlaySessionScreen.js
@@ -1,12 +1,50 @@
+//Components
 import Navbar from '../../Layouts/Navbar';
 import { UserContext } from '../../../context/userContext';
+import DndSheet from '../../Components/DndSheet'
 
+//Dependencies
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import {toast} from 'react-hot-toast'
 
 function PlaySession() {
+
+  const { user } = useContext(UserContext);
+	const navigate = useNavigate();
+	const [characterId, setUserId] = useState({
+    Id: ''
+  })
+
+  const fetchData = async () => {
+		try {
+			const characterId = window.location.pathname.split('/').pop();
+			setUserId({Id: characterId })
+			getCharacterData(characterId);
+		} catch (error) {
+			console.log(error)
+		}
+	}; useEffect(() => {
+		fetchData();
+	}, []);
+
+  const getCharacterData = async (characterId) => {
+		try {
+			const sentId = characterId ;
+			const number = '2'
+			const response = await axios.get('/CreateCharacter/' + number + '/' + sentId);
+			const characterData = response.data;
+			console.log('This is the character data:', JSON.stringify(characterData, null, 2));
+
+
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+
+
   return (
     <div style={{paddingBottom: '20px'}}>
 
@@ -26,9 +64,12 @@ function PlaySession() {
 
         </div>
 
-        <div className="col-5 d-flex justify-content-center align-items-center">
-          <header className="form-header text-center">Character Sheet</header>
+        <div className="col-5 row d-flex justify-content-center align-items-center">
+          <div className='col-12'>
+            <header className="form-header text-center">Character Sheet</header>
+          </div>
 
+        
         </div>
 
       </div>
