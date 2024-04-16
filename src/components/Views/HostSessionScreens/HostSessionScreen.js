3645c0fc68d18cd5b2a3b58d28ba30d7e21f6cca
Samuel Cole <samscreativespace@gmail.com>
2024-04-16 13:53:50 +0100

    setting up pop up

--- src/components/Views/HostSessionScreens/HostSessionScreen.js
+++ src/components/Views/HostSessionScreens/HostSessionScreen.js
@@ -17,17 +17,30 @@ function HostSession() {
         <Navbar navigationTitle="Session Menu" navigationTitleLink="/ChooseSession" secondNavigationTitle="Logout" navigationTitleSecondLink="/Login"/>
       </nav>
 
-      <div className='row mx-auto justify-content-center'  style={{paddingTop: '75px', maxWidth: '1900px', minWidth: '1500px', height: '97vh'}}>
-        <div className='col-9 row' >
+      <div style={{paddingTop: '75px', maxWidth: '100vw', minWidth: '1500px', height: '97vh', position: 'relative'}}>
 
-          <div className='col-12'>
-            <header className="form-header text-center">Map</header>
+        <div className='row mx-auto justify-content-center' style={{height: '100%'}}  >
+          <div className='col-9 row' >
+
+            <div className='col-12'>
+              <header className="form-header text-center">Map</header>
+            </div>
+
           </div>
 
+          <div className='col-3 row'>
+            <DiceRoller/>
+          </div>
         </div>
 
-        <div className='col-3 row'>
-          <DiceRoller/>
+        <div style={{position: 'absolute', top: '50%', right: 0, transform: 'translateY(-50%)', 
+        backgroundColor: 'transparent', cursor: 'pointer', zIndex: 999, height: '75%', width: '30%', display: 'flex', flexDirection: 'column'}}>
+            <div style={{height: '50%', backgroundColor: 'blue', width: '7%', alignSelf: 'flex-end'}}>
+                First Div
+            </div>
+            <div style={{height: '50%', backgroundColor: 'green', width: '7%', alignSelf: 'flex-end'}}>
+                Second Div
+            </div>
         </div>
 
       </div>
