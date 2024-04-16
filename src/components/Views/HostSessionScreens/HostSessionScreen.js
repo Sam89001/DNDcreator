ff53c60fe742f2b047afce352ad305288fd7c094
Samuel Cole <samscreativespace@gmail.com>
2024-04-16 13:18:31 +0100

    made dice roller a component

--- src/components/Views/HostSessionScreens/HostSessionScreen.js
+++ src/components/Views/HostSessionScreens/HostSessionScreen.js
@@ -1,6 +1,7 @@
 //Components
 import Navbar from '../../Layouts/Navbar';
 import { UserContext } from '../../../context/userContext';
+import DiceRoller from '../../Components/DiceRoller';
 
 //Dependencies
 import React, { useContext, useEffect, useState } from 'react';
@@ -15,6 +16,21 @@ function HostSession() {
       <nav className='navigation-bar'>
         <Navbar navigationTitle="Session Menu" navigationTitleLink="/ChooseSession" secondNavigationTitle="Logout" navigationTitleSecondLink="/Login"/>
       </nav>
+
+      <div className='row mx-auto justify-content-center'  style={{paddingTop: '75px', maxWidth: '1900px', minWidth: '1500px', height: '97vh'}}>
+        <div className='col-9 row' >
+
+          <div className='col-12'>
+            <header className="form-header text-center">Map</header>
+          </div>
+
+        </div>
+
+        <div className='col-3 row'>
+          <DiceRoller/>
+        </div>
+
+      </div>
 
     </div>
   )
