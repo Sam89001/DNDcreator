3145708db23e1104978ae9097a9296ced72f51f0
Samuel Cole <samscreativespace@gmail.com>
2024-04-16 18:19:42 +0100

    semi working grid

--- src/components/Views/HostSessionScreens/HostSessionScreen.js
+++ src/components/Views/HostSessionScreens/HostSessionScreen.js
@@ -1,6 +1,9 @@
 //CSS
 import '../../../css/Components.css'
 
+//Images
+import DiceImage from '../../../images/d20.png'
+
 //Components
 import Navbar from '../../Layouts/Navbar';
 import { UserContext } from '../../../context/userContext';
@@ -12,16 +15,89 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import {toast} from 'react-hot-toast'
 import ClipLoader from "react-spinners/ClipLoader";
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
 
 function HostSession() {
 
-  const [containerPopout, setActiveContainerPopOut] = useState(null);
+  //Popups
   const [popout, setActivePopOut] = useState(null);
+  const [lowerPopOut, setActiveLowerPopOut] = useState(null);
   const handlePopOut = (index) => {
-    setActivePopOut((prevPopout) => (prevPopout === index ? null : index));
-    setActiveContainerPopOut((prevPopout) => (prevPopout === index ? null : index));
+      setActivePopOut((prevPopout) => (prevPopout === index ? null : index));
   };
+  const handleLowerPopOut = (index) => {
+      setActiveLowerPopOut((prevPopout) => (prevPopout === index ? null : index));
+  };
+  function popoutContent() {
+    if(popout == null) {
+      return (
+        <div style={{padding: '5px'}}>
+          <img className='img-fluid' src={DiceImage} style={{opacity: '0.7'}}></img>
+        </div>
+      );
+    } else {
+      return (
+        <div>Place Characters Here</div>
+      );
+    }
+  }
+  function lowerPopoutContent() {
+    if(lowerPopOut == null) {
+      return (
+        <div style={{padding: '5px'}}>
+          <img className='img-fluid' src={DiceImage} style={{opacity: '0.7'}}></img>
+        </div>
+      );
+    } else {
+      return (
+        <div>2nd Test</div>
+      );
+    }
+  }
 
+  //Map Functions
+  const [userMapSize, setUserMapSize] = useState({
+    dimensionOne: '',
+    dimensionTwo: ''
+  })
+
+  function setMapSize() {
+  if (!userMapSize.dimensionOne || !userMapSize.dimensionTwo 
+    || isNaN(userMapSize.dimensionOne) || isNaN(userMapSize.dimensionTwo)) {
+    toast.error('Please include numbers for map size.');
+    return null;
+  }
+
+  const dimensionOne = parseInt(userMapSize.dimensionOne);
+  const dimensionTwo = parseInt(userMapSize.dimensionTwo);
+
+  const totalItems = dimensionOne * dimensionTwo;
+  const listItems = [];
+
+  for (let i = 1; i <= totalItems; i++) {
+    listItems.push(
+      <li key={`item-${i}`} style={{ display: 'inline-block', border: '1px solid grey', margin: 0, padding: 0, textAlign: 'center', width: 'calc(100% / ' + dimensionOne + ')' }}>
+        x{i !== totalItems ? ',' : ''} {/* Add comma after each item except the last one */}
+      </li>
+    );
+
+    // If the current item index is divisible by the dimensionOne, add a line break
+    if (i % dimensionOne === 0) {
+      listItems.push(<br key={`br-${i}`} />);
+    }
+  }
+
+  return (
+    <div style={{ backgroundColor: 'transparent', width: '100%', height: '100%', maxHeight: '700px', overflowY: 'hidden' }}>
+      <ul style={{ padding: 0, margin: 0, fontSize: '0.8vw' }}>
+        {listItems}
+      </ul>
+    </div>
+  );
+}
+
+  
+  
   return (
     <div style={{paddingBottom: '20px'}}>
       <nav className='navigation-bar'>
@@ -34,9 +110,39 @@ function HostSession() {
           <div className='col-9 row' >
 
             <div className='col-12'>
-              <header className="form-header text-center">Map</header>
+              List of turns will be here
             </div>
 
+            <div className='col-12' style={{height: '100%'}}>
+              
+              {/* List placed in here*/}
+              <div>
+                {setMapSize()}
+              </div>
+
+              <form onSubmit={(e) => { e.preventDefault(); setMapSize(); }}>
+                <label>Grid Size</label>
+                <div className='d-flex flex-row '>
+                  
+                    <input 
+                      onChange={(e) => setUserMapSize((prevData) => ({
+                        ...prevData,
+                        dimensionOne: e.target.value
+                    }))}/>
+                    <div>x</div>
+                    <input 
+                      onChange={(e) => setUserMapSize((prevData) => ({
+                        ...prevData,
+                        dimensionTwo: e.target.value
+                    }))}/>
+
+                    <button type="submit">Submit</button>
+                  
+                </div>
+              </form>
+
+            </div>
+            
           </div>
 
           <div className='col-3 row'>
@@ -46,17 +152,21 @@ function HostSession() {
         
  
         <div className={`host-popout ${popout === 0 ? 'active' : ''}`}
-          style={{ backgroundColor: 'var(--textGrey)', top: '30%' }}
-          onClick={() => handlePopOut(0)}>
-          Content 1
+            style={{ backgroundColor: 'var(--textGrey)', top: '20%' }}
+            onClick={() => handlePopOut(0)}>
+            <div>
+              {popoutContent()}
+            </div>
         </div>
 
-        <div className={`host-popout ${popout === 1 ? 'active' : ''}`}
-          style={{ backgroundColor: 'var(--lightBackgroundGrey)', bottom: '20%' }}
-          onClick={() => handlePopOut(1)}>
-          Content 2
+        <div className={`host-popout ${lowerPopOut === 1 ? 'active' : ''}`}
+            style={{ backgroundColor: 'var(--lightBackgroundGrey)', bottom: '20%' }}
+            onClick={() => handleLowerPopOut(1)}> {/* Changed index to 1 */}
+            <div>
+              {lowerPopoutContent()}  
+            </div>
         </div>
- 
+        
       </div>
 
     </div>
