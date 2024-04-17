a6755e24ca65138194fbd7c2061356125856c981
Samuel Cole <samscreativespace@gmail.com>
2024-04-17 18:07:23 +0100

    found way to put droppable context into html, need to find way to put draggable context intohtml

--- src/components/Views/HostSessionScreens/HostSessionScreen.js
+++ src/components/Views/HostSessionScreens/HostSessionScreen.js
@@ -32,6 +32,9 @@ function HostSession() {
   const handleLowerPopOut = (index) => {
       setActiveLowerPopOut((prevPopout) => (prevPopout === index ? null : index));
   };
+
+  
+  //Popup Content Rendering
   function popoutContent() {
     if(popout == null) {
       return (
@@ -50,13 +53,22 @@ function HostSession() {
                 <div className='text-center' style={{ display: 'block', width: '100%' }}>Default Characters</div>
                 
                 <div className='grid-counters-grid'>
-
-                  {stateCharacters.map((character) => (
-                    <div key={character.id} className='grid-counters'>
-                      <img className='img-fluid' src={character.image} style={{ width:'100%'}} />
-                    </div>
-                  ))}
-
+                  
+                    {stateCharacters.map((character, index) => (
+                      <Draggable key={character.id} draggableId={character.id} index={index}>
+                        {(provided) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            className='grid-counters'
+                          >
+                            <img className='img-fluid' src={character.image} style={{ width:'100%' }} />
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                  
                 </div>
 
               </div>
@@ -212,12 +224,15 @@ function HostSession() {
   ]
   const [stateCharacters, setStateCharacters] = useState(droppableCharacters)
 
-
+  
 
   //Map Functions
   const [userMapSize, setUserMapSize] = useState({
     dimensionOne: '',
     dimensionTwo: ''
+  })
+  const [userSquareHeight, setUserSquareHeight] = useState({
+    squareHeight: '',
   })
   useEffect(() => {
     setUserMapSize({ 
@@ -225,13 +240,10 @@ function HostSession() {
       dimensionTwo: '20'
      });
   }, []);
-
-  const [userSquareHeight, setUserSquareHeight] = useState({
-    squareHeight: '',
-  })
   useEffect(() => {
     setUserSquareHeight({ squareHeight: '1.5' });
   }, []);
+
 
   function setMapSize() {
   if (!userMapSize.dimensionOne || !userMapSize.dimensionTwo 
@@ -249,31 +261,20 @@ function HostSession() {
   for (let i = 1; i <= totalItems; i++) {
     listItems.push(
       <li key={`item-${i}`} style={{ display: 'inline-block', border: '1px solid grey', margin: 0, padding: 0, textAlign: 'center', width: 'calc(100% / ' + dimensionOne + ')' }}>
-        x{i !== totalItems ? ',' : ''} {/* Add comma after each item except the last one */}
+        x{i !== totalItems ? ',' : ''} 
       </li>
     );
 
-    // If the current item index is divisible by the dimensionOne, add a line break
     if (i % dimensionOne === 0) {
       listItems.push(<br key={`br-${i}`} />);
     }
   }
-  return (
-    <Droppable droppableId="droppable">
-        {(provided) => (
-          <div
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-            style={{ backgroundColor: 'transparent', width: '100%', height: '100%', maxHeight: '700px', overflowY: 'hidden' }}
-          >
-            <ul style={{ padding: 0, margin: 0, fontSize: `${userSquareHeight.squareHeight}vw` }}>
-              {listItems}
-            </ul>
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-  );
+  
+    return (
+       <ul style={{ padding: 0, margin: 0, fontSize: `${userSquareHeight.squareHeight}vw` }}>
+        {listItems}
+      </ul>
+    );
   }
 
   
@@ -283,110 +284,120 @@ function HostSession() {
         <Navbar navigationTitle="Session Menu" navigationTitleLink="/ChooseSession" secondNavigationTitle="Logout" navigationTitleSecondLink="/Login"/>
       </nav>
 
-      <div className="d-flex align-items-center" style={{paddingTop: '75px', maxWidth: '100vw', minWidth: '1500px', height: '97vh', }}>
+      <DragDropContext>
+        <div className="d-flex align-items-center" style={{paddingTop: '75px', maxWidth: '100vw', minWidth: '1500px', height: '97vh', }}>
 
-        <div className='row mx-auto justify-content-center' style={{height: '100%', width: '96%'}}  >
-          <div className='col-9 row' >
+          <div className='row mx-auto justify-content-center' style={{height: '100%', width: '96%'}}  >
+            <div className='col-9 row' >
 
-            <div className='col-12 d-flex justify-content-center flex-column' style={{paddingBottom: '5px'}}>
-              <div className="spells-field spell-form-titles text-center" style={{display: 'block', width: '100%', color: 'var(--textGrey)'}}> Initiative</div>
+              <div className='col-12 d-flex justify-content-center flex-column' style={{paddingBottom: '5px'}}>
+                <div className="spells-field spell-form-titles text-center" style={{display: 'block', width: '100%', color: 'var(--textGrey)'}}> Initiative</div>
 
-              <div className='d-flex flex-row justify-content-center' style={{width: '100%', paddingBottom: '10px'}}>
-                <div className='initiative-box d-flex justify-content-end align-items-center'>
-                  <img className='img-fluid' src={LeftArrow} style={{maxHeight: '2.5vh'}}></img>
-                </div>
-
-                <div className='initiative-box text-center'>Mike</div>
-                <div className='initiative-box text-center'>John</div>
-                <div className='initiative-box text-center'>Steve</div>
-                <div className='initiative-box text-center'>Jerm</div>
-                <div className='initiative-box text-center'>Farrah</div>
-
-                <div className='initiative-box d-flex justify-content-start align-items-center'>
-                  <img className='img-fluid' src={RightArrow} style={{maxHeight: '2.5vh'}}></img>
-                </div>
-              </div>
-
-              <div className="d-flex justify-content-center align-items-center flex-column" style={{ width: '100%'}}> 
-                <img className='img-fluid' src={UpArrow} style={{height: '1vh', width: '2vh'}}></img>
-                <div style={{fontSize: '1.5vh'}}>Turn Player</div>
-              </div>
-
-            </div>
-
-            <div className='col-12' style={{height: '100%'}}>
-              
-              {/* List placed in here*/}
-              <div style={{height: '100%', maxHeight: '630px', marginBottom: '10px'}}>
-                <DragDropContext>
-                  {setMapSize()}
-                </DragDropContext>
-              </div>
-
-              <div className='d-flex flex-row'>
-              {/* Grid Size*/}
-                <form onSubmit={(e) => { e.preventDefault(); setMapSize(); }}>
-                  <label>Grid Size</label>
-                  <div className='d-flex flex-row '>
-                    
-                      <input 
-                        onChange={(e) => setUserMapSize((prevData) => ({
-                          ...prevData,
-                          dimensionOne: e.target.value
-                      }))}/>
-                      <div>x</div>
-                      <input 
-                        onChange={(e) => setUserMapSize((prevData) => ({
-                          ...prevData,
-                          dimensionTwo: e.target.value
-                      }))}/>
-
-                      <button type="submit">Submit</button>
-                    
+                <div className='d-flex flex-row justify-content-center' style={{width: '100%', paddingBottom: '10px'}}>
+                  <div className='initiative-box d-flex justify-content-end align-items-center'>
+                    <img className='img-fluid' src={LeftArrow} style={{maxHeight: '2.5vh'}}></img>
                   </div>
-                </form>
-                
-                {/* Square Height*/}
-                <div className='d-flex flex-row '>
-                  <label>Square Height</label>
-                  <input 
-                    onChange={(e) => setUserSquareHeight((prevData) => ({
-                      ...prevData,
-                      squareHeight: e.target.value
-                    }))}/>
+
+                  <div className='initiative-box text-center'>Mike</div>
+                  <div className='initiative-box text-center'>John</div>
+                  <div className='initiative-box text-center'>Steve</div>
+                  <div className='initiative-box text-center'>Jerm</div>
+                  <div className='initiative-box text-center'>Farrah</div>
+
+                  <div className='initiative-box d-flex justify-content-start align-items-center'>
+                    <img className='img-fluid' src={RightArrow} style={{maxHeight: '2.5vh'}}></img>
+                  </div>
                 </div>
-                
+
+                <div className="d-flex justify-content-center align-items-center flex-column" style={{ width: '100%'}}> 
+                  <img className='img-fluid' src={UpArrow} style={{height: '1vh', width: '2vh'}}></img>
+                  <div style={{fontSize: '1.5vh'}}>Turn Player</div>
+                </div>
+
               </div>
 
+              <div className='col-12' style={{height: '100%'}}>
+                
+                {/* List placed in here*/}          
+                <div style={{height: '100%', maxHeight: '630px', marginBottom: '10px'}}>
+                  <Droppable droppableId="droppable">
+                    {(provided) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                        style={{ backgroundColor: 'transparent', width: '100%', height: '100%', maxHeight: '700px', overflowY: 'hidden' }}>
+                          {setMapSize()}
+                        {provided.placeholder}
+                      </div>
+                    )}
+                  </Droppable>
+                </div>  
+
+                <div className='d-flex flex-row'>
+                {/* Grid Size*/}
+                  <form onSubmit={(e) => { e.preventDefault(); setMapSize(); }}>
+                    <label>Grid Size</label>
+                    <div className='d-flex flex-row '>
+                      
+                        <input 
+                          onChange={(e) => setUserMapSize((prevData) => ({
+                            ...prevData,
+                            dimensionOne: e.target.value
+                        }))}/>
+                        <div>x</div>
+                        <input 
+                          onChange={(e) => setUserMapSize((prevData) => ({
+                            ...prevData,
+                            dimensionTwo: e.target.value
+                        }))}/>
+
+                        <button type="submit">Submit</button>
+                      
+                    </div>
+                  </form>
+                  
+                  {/* Square Height*/}
+                  <div className='d-flex flex-row '>
+                    <label>Square Height</label>
+                    <input 
+                      onChange={(e) => setUserSquareHeight((prevData) => ({
+                        ...prevData,
+                        squareHeight: e.target.value
+                      }))}/>
+                  </div>
+                  
+                </div>
+
+              </div>
+              
             </div>
-            
+
+            <div className='col-3 row'>
+              <DiceRoller/>
+            </div>
+          </div>
+          
+  
+          <div className={`host-popout ${popout === 0 ? 'active' : ''}`}
+              style={{ backgroundColor: 'var(--textGrey)', top: '20%' }}
+              onClick={() => handlePopOut(0)}>
+              <div className='d-flex' style={{ flex: '1', width: '90%' }}>
+                  {popoutContent()}
+              </div>
           </div>
 
-          <div className='col-3 row'>
-            <DiceRoller/>
+
+          <div className={`host-popout ${lowerPopOut === 1 ? 'active' : ''}`}
+              style={{ backgroundColor: 'var(--lightBackgroundGrey)', bottom: '20%' }}
+              onClick={() => handleLowerPopOut(1)}> {/* Changed index to 1 */}
+              <div className='d-flex' style={{ flex: '1', width: '90%' }}>
+                {lowerPopoutContent()}  
+              </div>
           </div>
+          
         </div>
-        
- 
-        <div className={`host-popout ${popout === 0 ? 'active' : ''}`}
-            style={{ backgroundColor: 'var(--textGrey)', top: '20%' }}
-            onClick={() => handlePopOut(0)}>
-            <div className='d-flex' style={{ flex: '1', width: '90%' }}>
-                {popoutContent()}
-            </div>
-        </div>
-
-
-        <div className={`host-popout ${lowerPopOut === 1 ? 'active' : ''}`}
-            style={{ backgroundColor: 'var(--lightBackgroundGrey)', bottom: '20%' }}
-            onClick={() => handleLowerPopOut(1)}> {/* Changed index to 1 */}
-            <div className='d-flex' style={{ flex: '1', width: '90%' }}>
-              {lowerPopoutContent()}  
-            </div>
-        </div>
-        
-      </div>
-
+      </DragDropContext>  
+                    
     </div>
   )
 }
