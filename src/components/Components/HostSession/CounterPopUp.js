c0e500d60a24ca44013ccc4a278365af504d31aa
Samuel Cole <samscreativespace@gmail.com>
2024-05-05 17:02:10 +0100

    further efficiency improvements

--- src/components/Components/HostSession/CounterPopUp.js
+++ src/components/Components/HostSession/CounterPopUp.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import {toast} from 'react-hot-toast'
 
 //Forms
 import UpdateCharacterForm from '../../Forms/HostSessionForms/UpdateCharacterForm'
@@ -14,28 +13,6 @@ function CounterPopUp({ setEditCharacterPopup, editCharacterPopup, placeCharacte
     }
   }, [droppedItem]);
 
-  const handleAuraSizeChange = (event) => {
-    const newValue = event.target.value;
-    setNewAuraSize(newValue); 
-  };
-
-  const updateAuraSize = () => {
-    const auraSizeNumber = parseFloat(newAuraSize);
-    if (isNaN(auraSizeNumber)) {
-      toast.error('Please enter a number');
-    } else if (auraSizeNumber > 999) {
-      toast.error('Value cannot exceed 999');
-    } else {
-      const updatedItems = droppedItems.map(item => {
-        if (item.uniqueId === placeCharacterEditableValues.uniqueId) {
-          return { ...item, auraSize: auraSizeNumber };
-        }
-        return item;
-      });
-      setDroppedItems(updatedItems);
-    }
-  };
-
   return (
     <div className={`d-flex align-items-center flex-column place-character-popup ${editCharacterPopup !== null ? 'active' : ''}`}>
       <div className='d-flex flex-end' style={{ width: '100%' }}>
@@ -45,8 +22,9 @@ function CounterPopUp({ setEditCharacterPopup, editCharacterPopup, placeCharacte
      {formSelection === 2 && <UpdateCharacterForm
         placeCharacterEditableValues={placeCharacterEditableValues}
         newAuraSize={newAuraSize}
-        handleAuraSizeChange={handleAuraSizeChange}
-        updateAuraSize={updateAuraSize}
+        setNewAuraSize={setNewAuraSize}
+        droppedItems={droppedItems}
+        setDroppedItems={setDroppedItems}
      />}
 
     </div>
--- src/components/Forms/HostSessionForms/UpdateCharacterForm.js
+++ src/components/Forms/HostSessionForms/UpdateCharacterForm.js
@@ -1,5 +1,28 @@
+import {toast} from 'react-hot-toast'
 
-function UpdateCharacterForm({ placeCharacterEditableValues, newAuraSize, updateAuraSize, handleAuraSizeChange }) {
+function UpdateCharacterForm({ placeCharacterEditableValues, newAuraSize, setNewAuraSize, droppedItems, setDroppedItems }) {
+
+  const handleAuraSizeChange = (event) => {
+    const newValue = event.target.value;
+    setNewAuraSize(newValue); 
+  };
+
+  const updateAuraSize = () => {
+    const auraSizeNumber = parseFloat(newAuraSize);
+    if (isNaN(auraSizeNumber)) {
+      toast.error('Please enter a number');
+    } else if (auraSizeNumber > 999) {
+      toast.error('Value cannot exceed 999');
+    } else {
+      const updatedItems = droppedItems.map(item => {
+        if (item.uniqueId === placeCharacterEditableValues.uniqueId) {
+          return { ...item, auraSize: auraSizeNumber };
+        }
+        return item;
+      });
+      setDroppedItems(updatedItems);
+    }
+  };
 
   return (
     <div>
