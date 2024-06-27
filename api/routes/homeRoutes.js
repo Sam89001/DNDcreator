c813f177a28e993245321d1d5628490d100ae7c9
Samuel Cole <samscreativespace@gmail.com>
2024-06-27 18:08:47 +0100

    redfined cors

--- api/routes/homeRoutes.js
+++ api/routes/homeRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const router = express.Router();
 
 const { hashedPassword, comparePassword } = require('../helpers/auth')
